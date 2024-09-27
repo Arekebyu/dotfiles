5bb651e006e5d828b6fdfe930844466732f16db2
antinomy <arekbyu@gmail.com>
2024-09-27 08:46:24 -0400

    I forgot what I did but it was something

--- nvim/lua/default/plugins/lazy.lua
+++ nvim/lua/default/plugins/lazy.lua
@@ -13,41 +13,52 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {'nvim-telescope/telescope.nvim', tag = '0.1.2',
-    dependencies = { 'nvim-lua/plenary.nvim' }},
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
     --{'rose-pine/neovim', name = 'rose-pine' },
     --{'vim-test/vim-test'},
-    {'AlexvZyl/nordic.nvim', name = 'nordic'},
-    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+    { 'AlexvZyl/nordic.nvim',               name = 'nordic' },
+    { 'nvim-treesitter/nvim-treesitter',    build = ':TSUpdate' },
     --eye candy
-    {'kyazdani42/nvim-web-devicons'},
-    {'yamatsum/nvim-nonicons'},
-    {'nvim-lualine/lualine.nvim'},
+    { 'kyazdani42/nvim-web-devicons' },
+    { 'yamatsum/nvim-nonicons' },
+    { 'nvim-lualine/lualine.nvim' },
     --github
-    {'tpope/vim-fugitive'},
+    { 'tpope/vim-fugitive' },
     --qol
-    {'ThePrimeagen/harpoon'},
-    {'mbbill/undotree'},
-    {'windwp/nvim-autopairs'},
-    {'windwp/nvim-ts-autotag'},
-    {'lukas-reineke/indent-blankline.nvim'},
-    {'folke/which-key.nvim'},
-    {'nvim-tree/nvim-tree.lua'},
-    {'norcalli/nvim-colorizer.lua'},
+    { 'ThePrimeagen/harpoon' },
+    { 'mbbill/undotree' },
+    { 'windwp/nvim-autopairs' },
+    { 'windwp/nvim-ts-autotag' },
+    { 'lukas-reineke/indent-blankline.nvim' },
+    { 'folke/which-key.nvim' },
+    { 'nvim-tree/nvim-tree.lua' },
+    { 'norcalli/nvim-colorizer.lua' },
     --lsp
-    {'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {'williamboman/mason.nvim'},           -- Optional
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
         }
     },
-    {'simrat39/rust-tools.nvim'},
+    {
+        'lervag/vimtex',
+        lazy = false,
+        init = function()
+            vim.g.vimtex_view_method = "zathura"
+        end
+    },
+    { 'simrat39/rust-tools.nvim' },
 })
